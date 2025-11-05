e3ed90c232067193f282c4940ca62a8726dfc9ee
tai532005 <157967905+tai532005@users.noreply.github.com>
2025-11-06 00:21:15 +0700

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Landing from "../pages/Landing.jsx";
+import Login from "../pages/auth/Login.jsx";
+import Register from "../pages/auth/Register.jsx";
+import VerifyOtp from "../pages/auth/VerifyOtp.jsx";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />            {/* landing */}
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/verify" element={<VerifyOtp />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
