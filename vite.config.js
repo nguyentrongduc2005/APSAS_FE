fb940b38df11deeb235c14346697158d58062555
Duc <nguyentrongduc447@gmail.com>
2025-11-26 22:55:44 +0700

    check

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 /* eslint-env node */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-  "@": path.resolve('.', "./src"),
+      "@": path.resolve(".", "./src"),
     },
   },
-   server: {
+  server: {
     port: 5173,
     proxy: {
       "/api": {
@@ -22,4 +22,34 @@ export default defineConfig({
       },
     },
   },
-})
+});
+/* eslint-env node */
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(".", "./src"),
+//     },
+//   },
+//   server: {
+//     // --- THÊM DÒNG NÀY ĐỂ SỬA LỖI TUNNEL ---
+//     allowedHosts: true,
+//     // ---------------------------------------
+
+//     port: 5173,
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:8080", // BE của bạn
+//         changeOrigin: true,
+//         // không rewrite vì backend đã có context-path /api
+//         // /api/auth/register -> http://localhost:8080/api/auth/register
+//       },
+//     },
+//   },
+// });
